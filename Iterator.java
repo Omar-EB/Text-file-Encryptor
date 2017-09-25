123fe2b9c12d497c60b49a2299d9c57a10589342
Omar-EB <32253541+Omar-EB@users.noreply.github.com>
2017-09-25 00:31:36 -0400

    Update Iterator.java

--- Iterator.java
+++ Iterator.java
@@ -1,3 +1,6 @@
+//Author : Omar Elboraey
+//Usage of the following code without the consent of Omar Elboraey is prohibited
+
 import java.util.NoSuchElementException;
 
 public interface Iterator<E> {
